fb0709482fcec01a4d0d896405e9396eaf744566
Bizizou <thestrikeur2@gmail.com>
2021-07-27 15:40:36 +0200

    cafaitplaiz

--- resources/views/tables/admin.blade.php
+++ resources/views/tables/admin.blade.php
@@ -7,17 +7,29 @@
         <th scope="col">Mot de passe</th>
         <th scope="col">role</th>
         <th scope="col">Avatar</th>
+        <th scope="col">Action</th>
       </tr>
     </thead>
     <tbody>
         @foreach ($admins as $admin)
             <tr>
-                <th scope="row">{{$admin->id}}</th>
+                <th scope="row">{{$admin->id}}</th>   
                 <td>{{$admin->username}}</td>
                 <td>{{$admin->email}}</td>
                 <td>{{$admin->password}}</td>
                 <td>{{$admin->role}}</td>
                 <td>{{$admin->avatar_name}}</td>
+                <td>
+                  <div class="dropdown">
+                      <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                          <i class="dw dw-more"></i>
+                      </a>
+                      <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                          <a class="dropdown-item" ><i class="dw dw-eye"></i> Modifier</a>
+                          <a class="dropdown-item" ><i class="dw dw-download"></i> Supprimer</a>
+                      </div>
+                  </div>
+                </td>
                 
             </tr>
 
